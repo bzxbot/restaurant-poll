8a2b215da3902208f2903d96c29ab5ece897fd5a
Bernardo Botelho <botelho.bernardo@gmail.com>
2015-04-10 19:57:27 -0300

    Last changes and README

--- RestaurantPoll.Tests/Models/DayTest.cs
+++ RestaurantPoll.Tests/Models/DayTest.cs
@@ -23,6 +23,8 @@ namespace RestaurantPoll.Tests.Models
         [TestMethod]
         public void GetHighestVotedTest()
         {
+            // Estória 1 e 3.
+            // Restaurante mais votado.
             var day = new Day(new DateTime(2015, 12, 12), null);
             var joao = new User(1, "joao@dbserver.com", "db");
             var maria = new User(2, "maria@dbserver.com", "db");
@@ -34,12 +36,16 @@ namespace RestaurantPoll.Tests.Models
             day.Vote(joao, panorama);
             day.Vote(maria, panorama);
             day.Vote(jose, intervalo);
+            // João e Maria votam novamente, o vencedor continua sendo Panorama.
+            day.Vote(joao, intervalo);
+            day.Vote(maria, intervalo);
             Assert.AreEqual(day.GetHighestVoted().Id, panorama.Id);
         }
 
         [TestMethod]
         public void GetHighestVotedCountTest()
         {
+            // Restaurante com maior número de votos.
             var day = new Day(new DateTime(2015, 12, 12), null);
             var joao = new User(1, "joao@dbserver.com", "db");
             var maria = new User(2, "maria@dbserver.com", "db");
@@ -52,6 +58,28 @@ namespace RestaurantPoll.Tests.Models
             day.Vote(maria, panorama);
             day.Vote(jose, intervalo);
             Assert.AreEqual(day.GetHighestVotedCount(), 2);
+        }
+
+        [TestMethod]
+        public void SetRestaurantsTest()
+        {
+            // Estória 2.
+            // Um restaurante escolhido em um dia anterior não pode ser escolhido novamente.
+            var day = new Day(new DateTime(2015, 12, 12), null);
+            var joao = new User(1, "joao@dbserver.com", "db");
+            var maria = new User(2, "maria@dbserver.com", "db");
+            var jose = new User(3, "jose@dbserver.com", "db");
+            // Todos os restaurantes.
+            day.SetRestaurants();
+            var panorama = new Restaurant(1, "Panorama");
+            var intervalo = new Restaurant(2, "Intervalo 50");
+            Assert.AreEqual(day.Restaurants.Count, 5);
+            day.Vote(joao, panorama);
+            day.Vote(maria, panorama);
+            day.Vote(jose, intervalo);
+            var nextDay = new Day(new DateTime(2015, 12, 13), day);
+            nextDay.SetRestaurants();
+            Assert.AreEqual(nextDay.Restaurants.Count, 4);
         }
     }
 }
---
+++ RestaurantPoll/README.md
@@ -0,0 +1,47 @@
+Restaurant Poll
+-------
+
+Um pequeno aplicativo para decidir onde realizar o almoço da DBServer.
+
+O aplicativo possui um formulário de login. O formulário de login, conforme instruções dos requisitos
+foi feito sem utilizar banco de dados. Os usuários são definidos quando a aplicação é executada na classe User.
+
+Os usuários criados por padrão são os seguintes:
+
+> joao@dbserver.com
+ 
+> maria@dbserver.com
+
+> jose@dbserver.com
+
+A interface do sistema foi desenvolvida utilizando Bootstrap.
+
+Para todos, a senha é "**db**", sem as aspas.
+
+O objetivo do formulário de login é apenas manter um registro dos votos nos restaurantes, e não criar um
+formulário de login seguro e robusto. Se esse fosse o caso, eu utilizaria os recursos prontos do ASP.NET MVC 5. Porém,
+como a orientação era a não utilização de um banco de dados, optei por uma versão light para autenticação.
+
+Após a tela de login, é exibido uma lista com cinco restaurantes pré-cadastrados para que o usuário possa fazer sua escolha.
+
+A data padrão do sistema é sempre o dia atual. Como os requisitos não são claro, a semana só tem dias uteis, de segunda até sexta-feira.
+
+O sistema permite, para facilitar testes, que a data seja avançada para o próximo dia útil.
+
+Um ponto que não é esclarecido nos requisitos é qual o critério para desempate da votação.
+
+Como não é possível esclarer os requisitos, o sistema escolhe o primeiro restaurante com mais votos, de acordo com a estrutura interna do sistema.
+
+Os testes unitários cobrem as 3 estórias, e mais alguns métodos imporantes do sistema.
+
+Instalação
+-------
+
+Obtenha o código fonte e execute através do Visual Studio.
+
+O projeto foi desenvolvido usando Visual Studio 2013 Community Edition.
+
+Autor
+-------
+
+Bernardo Botelho
